a2c9e4ff1ef892d1130d2de7b12fef9ba234bf71
mikholi <keranemi@icloud.com>
2025-02-02 09:54:03 +0200

    created navigation

---
+++ src/navigation/Navigation.js
@@ -0,0 +1,47 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { View } from "react-native";
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+
+
+
+const Tab = createBottomTabNavigator();
+
+const HOME = 'Home';
+const PROFILE = 'Profile'
+const EXERCISES = 'Exercises'
+
+const icons = {
+    [HOME]: 'home',
+    [PROFILE]: 'message1',
+    [EXERCISES]: 'setting'
+}
+
+export default function BottomNav() {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen
+                    name={HOME}
+                    component={HomeScreen}
+                    options={{tabBarIcon: ()=> <AntDesign name={icons[HOME]}size={20}/>}}
+                />
+                <Tab.Screen
+                    name={PROFILE}
+                    component={ProfileScreen}
+                    options={{tabBarIcon: () => <AntDesign name={icons[PROFILE]}size={20}/>}}
+                />
+                <Tab.Screen
+                    name={EXERCISES}
+                    component={ExercisesScreen}
+                    options={{tabBarIcon: () => <AntDesign name={icons[EXERCISES]}size={20}/>}}
+                />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
+
+const HomeScreen = () => <View style={{backgroundColor: 'yellow', flex:1}}/>;
+const ProfileScreen = () => <View style={{backgroundColor: 'blue', flex: 1}}/>
+const ExercisesScreen = () => <View style={{backgroundColor: 'red', flex: 1}}/>
